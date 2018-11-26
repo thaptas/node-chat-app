88cd40c08b27c88cb4000340a9f19a4c9a6e7341
Tomasz Haptas <tomasz.haptas@rapidcodefactory.com>
2018-11-26 15:24:40 +0100

    socket.io init

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 require('./../config/config.js');
 
+const http = require('http');
 const _ = require('lodash');
 const express = require('express');
+const socketIO = require('socket.io');
+
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
@@ -9,6 +12,10 @@ const path = require('path');
 const publicPath = path.join(__dirname, '../public');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+
 const port = process.env.PORT || 3000;
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -19,7 +26,16 @@ app.use(bodyParser.json());
 //middleware set static folder
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+  console.log('New user connected');
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  })
+});
 
-app.listen(port, () => {
+
+
+
+server.listen(port, () => {
   console.log('Server is up on port ', port)
 });
